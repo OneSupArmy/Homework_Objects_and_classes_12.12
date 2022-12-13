f72fbaebdb9075a34d4bcfac167d96c43ae5a117
OneSupArmy <saska.jukoff@gmail.com>
2022-12-13 15:51:24 +0300

    homework about Classes and Objects - done

---
+++ src/Book.java
@@ -0,0 +1,22 @@
+public class Book {
+    private String bookName;
+    private Author author;
+    private int yearOfPublishing;
+    public Book(String bookName, Author author, int yearOfPublishing) {
+        this.bookName = bookName;
+        this.author = author;
+        this.yearOfPublishing = yearOfPublishing;
+    }
+    public String getBookName() {
+        return bookName;
+    }
+    public Author getAuthor() {
+        return author;
+    }
+    public int getYearOfPublishing() {
+        return yearOfPublishing;
+    }
+    public void setYearOfPublishing(int yearOfPublishing) {
+        this.yearOfPublishing = yearOfPublishing;
+    }
+}
